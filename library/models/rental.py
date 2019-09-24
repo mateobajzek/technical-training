a8917a236b2fb9e2c45fa99ae11b4a5d098982e8
mateo <mateovk007@gmail.com>
2019-09-24 10:38:21 +1000

    FIX#131

--- library/models/rental.py
+++ library/models/rental.py
@@ -15,7 +15,7 @@ class Rentals(models.Model):
     book_id = fields.Many2one('library.book', string='Book')
 
     book_isbn = fields.Char(string='ISBN', related='book_id.isbn')
-    book_author_ids = fields.Many2many("library.partner", string="Authors", related="book_ids.author_ids")
+    book_author_ids = fields.Many2many("library.partner", string="Authors", related="book_id.author_ids")
 
     rental_date = fields.Date()
     return_date = fields.Date()
